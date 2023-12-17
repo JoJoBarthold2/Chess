7fbd92458f80c078aa002264efb525fd0c26c7fa
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-12-17 16:54:54 +0100

    testing is working !

--- test_pytest.py
+++ test_pytest.py
@@ -1,2 +1,11 @@
-import student_agents.template_agent as template_agent
+import student_agents.template as template_agent
+import ChessEngine 
+
+checkkmate = ChessEngine.GameState()
+checkkmate.checkMate = True
+
+
+def test_heuristic():
+ assert template_agent.Agent.simpleHeuristic(template_agent.Agent(),checkkmate) == -float("inf")
+
 
